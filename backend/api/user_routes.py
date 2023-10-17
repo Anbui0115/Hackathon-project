be3e2fd7d528b4e031c82c1fcde8c32f8055cc21
Wang55566 <wang6@hawaii.edu>
2023-10-17 14:29:12 -0700

    flask setup

---
+++ backend/api/user_routes.py
@@ -0,0 +1,2 @@
+from flask_login import login_required
+from backend.models import db, User
---
+++ backend/models/user.py
@@ -0,0 +1,2 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
