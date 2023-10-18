e1f915ae022ab4403385a963944ac6a6c9a732c8
An Bui <anbui@Eddies-MacBook-Pro-2.local>
2023-10-17 19:55:24 -0700

    style footer, need to finalize on what info should be in the footer so it's clean, brief and informative

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import React from 'react';
 
+// TO DO : style footer
+
 const footerStyle = {
   backgroundColor: '#333',
   color: '#fff',
@@ -14,10 +16,6 @@ const columnStyle = {
   margin: '0 auto',
 };
 
-const socialIconsStyle = {
-  listStyle: 'none',
-  padding: 0,
-};
 
 const pageStyle = {
   minHeight: '100vh',  // Ensure the page height covers the viewport.
@@ -32,8 +30,8 @@ const Footer = () => {
       <div className="container" style={columnStyle}>
         <div className="row">
           <div className="col-md-4">
-            <h3>Contact Us</h3>
-            <p>If you have any questions or need assistance, please don't hesitate to reach out.</p>
+            {/* <h3>Contact Us</h3>
+            <p>If you have any questions or need assistance, please don't hesitate to reach out.</p> */}
             <p>Address: Priyada Arts Studio, Newark </p>
             <p>
               <a href='https://m.facebook.com/priyankaraghuramandance'>Facebook</a>
@@ -49,14 +47,14 @@ const Footer = () => {
             <p>Phone: (123) 456-7890</p>
           </div>
 
-          <div className="col-md-4">
+          {/* <div className="col-md-4">
             <h3>Quick Links</h3>
             <ul style={socialIconsStyle}>
               <li><a href="/">Home</a></li>
               <li><a href="/classes">Classes</a></li>
               <li><a href="/about">About Us</a></li>
             </ul>
-          </div>
+          </div> */}
         </div>
 
         <hr />
