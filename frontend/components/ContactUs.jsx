1dd7c1b6fdba95ca9e9296cc4d691582722090f9
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-10-21 18:38:04 -0700

    Login / Sign Up Page Done CSS Cleaned Up

--- frontend/components/ContactUs.jsx
+++ frontend/components/ContactUs.jsx
@@ -1,10 +1,13 @@
 "use client"
+import { useState, useEffect } from "react"
 
 const ContactUs = ({toggleModal, setToggleModal}) => {
 
   const handleCloseModal = () => {
     setToggleModal(false)
   }
+
+
 
   return (
     <div onClick={(e) => e.stopPropagation()} className="z-10 absolute left-[30%] top-[15%] h-[auto] bg-white w-[40%] p-10 border-2 rounded-md shadow-2xl">
@@ -47,6 +50,19 @@ const ContactUs = ({toggleModal, setToggleModal}) => {
             cols="50"  // this sets the width of the textarea
           ></textarea>
         </label>
+
+        <div className='py-6 flex gap-4 justify-center'>
+          <button 
+            className="bg-orange-500 hover:bg-gray-600 text-white rounded-md p-2 transition duration-700"
+            type="submit"
+          >
+            Submit
+          </button>
+
+          <button className="bg-orange-500 text-white rounded-md p-2 hover:bg-gray-600 transition duration-700">
+            Cancel
+          </button>
+        </div>
         
       </form>
     </div>
