563b6996b0b9560e716a90e3898c3ae142562e7d
Keerthana Yellapragada <keerthanay@gmail.com>
2023-10-24 11:09:43 -0700

    general appointment routes

--- backend/api/general_appointments_routes.py
+++ backend/api/general_appointments_routes.py
@@ -1,90 +1,90 @@
-# from flask import Blueprint, render_template, url_for, redirect, request, jsonify
-# from ..models import GeneralAppointment, User, db
-# from flask_login import current_user, login_user, logout_user, login_required
-# from ..forms.general_appointment_form import CreateGeneralAppointmentForm
+from flask import Blueprint, render_template, url_for, redirect, request, jsonify
+from ..models import GeneralAppointment, User, db
+from flask_login import current_user, login_user, logout_user, login_required
+from ..forms.general_appointment_form import CreateGeneralAppointmentForm
 
 
-# # ____________________________________________________________________________________________________________________
+# ____________________________________________________________________________________________________________________
 
-# general_appointment_bp = Blueprint(
-#     "general_appointments_routes", __name__, url_prefix="/api/generalappointments")
-
-
-# # ____________________________________________________________________________________________________________________
-
-# # GET ALL GENERAL APPOINTMENTS
+general_appointment_bp = Blueprint(
+    "general_appointments_routes", __name__, url_prefix="/api/generalappointments")
 
 
-# @general_appointment_bp.route("/", methods=["GET"])
-# def all_general_appointments():
-#     general_appointments = General_Appointment.query.all()
-#     response = []
-#     if general_appointments:
-#         for apt in general_appointments:
-#             apt_obj = apt.to_dict()
-#             response.append(apt_obj)
-#         print("THIS IS general_appointments FROM BACKEND", response)
-#         return {
-#             "general_appointments": response
-#         }, 200
-#     return {"Error": "Appointment Not Found"}, 404
+# ____________________________________________________________________________________________________________________
+
+# GET ALL GENERAL APPOINTMENTS
 
 
-# # ____________________________________________________________________________________________________
-
-# # CREATE A GENERAL APPOINTMENT
-
-# @general_appointment_bp.route("/new/", methods=["POST"])
-# @login_required
-# def create_general_apt():
-
-#     create_general_apt_form = CreateGeneralAppointmentForm()
-#     create_general_apt_form['csrf_token'].data = request.cookies['csrf_token']
-#     # print("current user is: **********************************", current_user)
-
-#     if create_general_apt_form.validate_on_submit():
-#         data = create_general_apt_form.data
-#         new_general_apt = General_Appointment(
-#             # name=data["name"],
-#             user_id=current_user.id,
-#             type=data["type"],
-#             date=data["class"],
-#             duration=data["duration"],
-#             location=data["location"],
-#             notes=data["notes"],
-#             accepted=False
-#         )
-
-#         db.session.add(new_general_apt)
-#         db.session.commit()
-
-#         new_general_apt_obj = new_general_apt.to_dict()
-#         return new_general_apt_obj, 201
+@general_appointment_bp.route("/", methods=["GET"])
+def all_general_appointments():
+    general_appointments = GeneralAppointment.query.all()
+    response = []
+    if general_appointments:
+        for apt in general_appointments:
+            apt_obj = apt.to_dict()
+            response.append(apt_obj)
+        print("THIS IS general_appointments FROM BACKEND", response)
+        return {
+            "general_appointments": response
+        }, 200
+    return {"Error": "Appointment Not Found"}, 404
 
 
-# # __________________________________________________________________________________________________
+# ____________________________________________________________________________________________________
+
+# CREATE A GENERAL APPOINTMENT
+
+@general_appointment_bp.route("/new/", methods=["POST"])
+@login_required
+def create_general_apt():
+
+    create_general_apt_form = CreateGeneralAppointmentForm()
+    create_general_apt_form['csrf_token'].data = request.cookies['csrf_token']
+    # print("current user is: **********************************", current_user)
+
+    if create_general_apt_form.validate_on_submit():
+        data = create_general_apt_form.data
+        new_general_apt = GeneralAppointment(
+            # name=data["name"],
+            user_id=current_user.id,
+            type=data["type"],
+            date=data["class"],
+            duration=data["duration"],
+            location=data["location"],
+            notes=data["notes"],
+            accepted=False
+        )
+
+        db.session.add(new_general_apt)
+        db.session.commit()
+
+        new_general_apt_obj = new_general_apt.to_dict()
+        return new_general_apt_obj, 201
 
 
-# # DELETE A GENERAL APPOINTMENT
+# __________________________________________________________________________________________________
 
 
-# @general_appointment_bp.route("/<int:general_apt_id_id>/", methods=["DELETE"])
-# def delete_general_apt(general_apt_id):
-
-#     current_general_apt = General_Appointment.query.get(general_apt_id)
-
-#     if current_general_apt:
-#         db.session.delete(current_general_apt)
-#         db.session.commit()
-#         return "Your appointment was succesfully deleted"
-
-#     return "404 review not found"
+# DELETE A GENERAL APPOINTMENT
 
 
-# # function to convert datetime obj to integer value so we can compare
+@general_appointment_bp.route("/<int:general_apt_id_id>/", methods=["DELETE"])
+def delete_general_apt(general_apt_id):
 
-# def to_integer(date_time):
-#     return 10000*date_time.year + 100*date_time.month + date_time.day
+    current_general_apt = GeneralAppointment.query.get(general_apt_id)
+
+    if current_general_apt:
+        db.session.delete(current_general_apt)
+        db.session.commit()
+        return "Your appointment was succesfully deleted"
+
+    return "404 review not found"
 
 
-# # _____________________________________________________________________________________
+# function to convert datetime obj to integer value so we can compare
+
+def to_integer(date_time):
+    return 10000*date_time.year + 100*date_time.month + date_time.day
+
+
+# _____________________________________________________________________________________
