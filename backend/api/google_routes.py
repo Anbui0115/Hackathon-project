b77a4716f67d359760b5d47a4983ed9b89a21033
Wang55566 <wang6@hawaii.edu>
2023-10-26 12:51:31 -0700

    solved logout problem

--- backend/api/google_routes.py
+++ backend/api/google_routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, session, redirect, url_for, Blueprint
 from ..oauth_config import oauth
 from ..models import db, User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, login_required
 import requests
 
 google_routes = Blueprint('google', __name__)
@@ -15,7 +15,7 @@ def googleLogin():
 @google_routes.route('/callback')
 def googleCallback():
     token = oauth.dance_class_app.authorize_access_token()
-    session['user'] = token
+    session['google'] = token
     user = User.query.filter_by(email=token['userinfo']['email']).first()
     if not user:
         new_user = User(
@@ -41,16 +41,21 @@ def googleCallback():
 @google_routes.route('/display_data')
 @login_required
 def display_data():
-    # if session.get('user') is None:
-    #     return redirect(url_for('google.googleLogin'))
-    return session.get('user')
+    if session.get('google') is None:
+        return redirect(url_for('google.googleLogin'))
+    return session.get('google')
 
 # Logout
 @google_routes.route('/logout')
 @login_required
 def logout():
     requests.post('https://oauth2.googleapis.com/revoke',
-    params={'token': session['user']['access_token']},
+    params={'token': session['google']['access_token']},
     headers = {'content-type': 'application/x-www-form-urlencoded'})
-    # logout_user()
-    return redirect(url_for('google.googleLogin'))
+    return redirect('/api/auth/logout')
+
+# View the session data
+@google_routes.route('/view_session')
+def view_session():
+    session_data = session.copy()  # Create a copy of the session data
+    return str(session_data)
