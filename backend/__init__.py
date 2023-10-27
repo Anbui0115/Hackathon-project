d97192224668b0aa38d32f6167588699d832a270
Keerthana Yellapragada <keerthanay@gmail.com>
2023-10-26 18:18:09 -0700

    debugging

--- backend/__init__.py
+++ backend/__init__.py
@@ -20,7 +20,7 @@ import os
 from authlib.integrations.flask_client import OAuth
 from backend.oauth_config import configure_oauth
 from .api.google_routes import google_routes
-from .api.media_routes import media_bp
+from .api.media_routes import media_routes
 
 app = Flask(__name__)
 app.cli.add_command(seed_commands)
@@ -43,7 +43,7 @@ app.register_blueprint(about_routes, url_prefix='/api/about')
 app.register_blueprint(billing_bp, url_prefix='/api/billing')
 app.register_blueprint(google_routes, url_prefix='/api/google')
 app.register_blueprint(testimonial_bp, url_prefix='/api/testimonials')
-app.register_blueprint(media_bp, url_prefix='/api/media')
+app.register_blueprint(media_routes, url_prefix='/api/media')
 
 
 
