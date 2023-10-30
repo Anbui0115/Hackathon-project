050c12283983cecd05304e6ef9123fbc64ef54ab
Dilang <dilang@berkeley.edu>
2023-10-30 15:56:06 -0700

    fix testimonial creation
    merge main

--- frontend/components/CreateTestimonial.jsx
+++ frontend/components/CreateTestimonial.jsx
@@ -28,6 +28,7 @@ const CreateTestimonial = ({ isOpen, onRequestClose }) => {
 
     const handleSubmit = async (e) => {
       e.preventDefault();
+      e.preventDefault();
       try {
         const response = await axios.post('http://127.0.0.1:5000/api/testimonials/', { 
           first_name: firstName,
