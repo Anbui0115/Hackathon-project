cdec5d157ab94235083dc499be6810071e52f9fd
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-10-31 13:20:56 -0700

    Gallery Mostly Done

--- frontend/components/ChatBot.jsx
+++ frontend/components/ChatBot.jsx
@@ -36,8 +36,10 @@ const ChatBot = () => {
   };
 
   useEffect(() => {
-    scrollToBottom();
-    hideWhiteCloudMessage(); 
+    if (scrollToBottom()) {
+      hideWhiteCloudMessage(); 
+    };
+    
   }, [messages]);
 
   const sendMessage = async (e) => {
@@ -139,7 +141,7 @@ const ChatBot = () => {
       {/* White Cloud Message */}
       {!toggleChat && showWhiteCloudMessage  && (  
         <div className="relative p-4 m-2 tracking-wider bg-white font-semibold text-black rounded-3xl shadow-md w-[240px] h-auto">
-          <AnimatedText timer={15} message="Hi! PriyadaGPT at your service click me if you have any questions about the school, artists, classes, or about the website and I'd be glad to help." />
+          <AnimatedText timer={15} message="Hi! PriyadaGPT here. Click me if you need help or have any questions." />
         </div>
       )}
 
