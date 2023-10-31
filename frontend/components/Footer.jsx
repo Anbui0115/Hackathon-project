7dd93fc5b96b21ce1283d843b19bb99db5282f5d
Keerthana Yellapragada <keerthanay@gmail.com>
2023-10-31 11:57:16 -0700

    CSS changes requested by clinet

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -74,7 +74,7 @@ const Footer = () => {
 
               <div className="flex gap-2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-locate-fixed"><line x1="2" x2="5" y1="12" y2="12"/><line x1="19" x2="22" y1="12" y2="12"/><line x1="12" x2="12" y1="2" y2="5"/><line x1="12" x2="12" y1="19" y2="22"/><circle cx="12" cy="12" r="7"/><circle cx="12" cy="12" r="3"/></svg>
-                <div className="text-cream text-sm font-medium">Address: Priyada Arts Studio, Newark, California</div>
+                <div className="text-cream text-sm font-medium">Priyada Arts Studio, Newark, CA</div>
               </div>
         </div>
 
