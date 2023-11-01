d2f0702effd5aebc7f490756b137e10d546c39fc
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-11-01 16:42:14 -0700

    testing

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,6 +12,17 @@ const nextConfig = {
         pathname: "/"
       }
     ]
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: 
+        process.env.NODE_ENV === 'development'
+        ? 'http://127.0.0.1:8000/api/:path*'
+        : '/api/',
+      },
+    ]
   }
 }
 
