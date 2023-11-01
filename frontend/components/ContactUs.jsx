b9e6948e02391ded592680ac5db64ddb6735f790
Keerthana Yellapragada <keerthanay@gmail.com>
2023-11-01 15:35:41 -0700

    contact us to match the rest of the forms

--- frontend/components/ContactUs.jsx
+++ frontend/components/ContactUs.jsx
@@ -1,86 +1,72 @@
-"use client"
+import { useState } from "react";
 
-
-import { useState, useEffect } from "react"
-
-const ContactUs = ({toggleModal, setToggleModal}) => {
-
+const ContactUs = ({ toggleModal, setToggleModal }) => {
   const handleCloseModal = () => {
-    setToggleModal(false)
-  }
-
-
+    setToggleModal(false);
+  };
 
   return (
-    <div onClick={(e) => e.stopPropagation()} className="z-200 fixed left-[35%] mb-40 top-[25%] h-[auto] bg-gray-700 text-white sm:text-md lg:text-lg w-[30%] p-7  rounded-md">
-
+    <div onClick={(e) => e.stopPropagation()} className="z-200 fixed left-1/3 mb-40 top-1/4 bg-white text-black w-full sm:w-1/2 md:w-1/3 p-7 rounded-md shadow-lg">
       <div className="flex justify-between items-center">
-        <h1 className="text-xl font-bold mb-2">Contact Us</h1>
-        <button className="" onClick={() => handleCloseModal()} >X</button>
+        <h1 className="text-xl sm:text-2xl font-bold mb-2">Contact Us</h1>
+        <button className="text-black" onClick={() => handleCloseModal()}>
+          X
+        </button>
       </div>
-      <span className="">We would love to hear from you!</span>
+      <span className="text-black">We would love to hear from you!</span>
 
+      <hr className="my-4" />
 
-      <hr className="mx-[-1.75rem] my-4" />
-      <form
-        className=""
-        action=""
-      >
-
-        <label className="flex flex-col gap-2 my-2"  htmlFor="">
+      <form action="">
+        <label className="flex flex-col gap-2 my-2" htmlFor="">
           Name
           <input
             type="text"
-            placeholder=""
-            className="outline-none text-black border-2 border-none rounded-md p-2 "
+            placeholder="Enter your name"
+            className="text-black border rounded-md p-2 border-gray-300 focus:border-gray-400"
           />
         </label>
 
-        <label className="flex flex-col gap-2 my-2"  htmlFor="">
+        <label className="flex flex-col gap-2 my-2" htmlFor="">
           Email
           <input
             type="email"
-            placeholder=""
-            className="outline-none text-black border-2 border-none rounded-md p-2"
+            placeholder="Enter your email"
+            className="text-black border rounded-md p-2 border-gray-300 focus:border-gray-400"
           />
         </label>
 
-
-        <label className="flex text-white flex-col gap-2 my-2"  htmlFor="">
+        <label className="flex text-black flex-col gap-2 my-2" htmlFor="">
           Subject Line
           <input
             type="text"
-            placeholder=""
-            className="outline-none border-2 text-black border-none rounded-md p-2"
+            placeholder="Enter subject"
+            className="text-black border rounded-md p-2 border-gray-300 focus:border-gray-400"
           />
         </label>
 
-        <label className="flex flex-col gap-2"  htmlFor="">
+        <label className="flex flex-col gap-2" htmlFor="">
           Message to Priyada
           <textarea
-            placeholder="Type your message to the Priyada here"
-            className="outline-none border-2 text-black border-none resize-none  rounded-md p-3"
-            rows="5"   // this sets the height of the textarea
-            cols="50"  // this sets the width of the textarea
+            placeholder="Type your message to Priyada here"
+            className="text-black border resize-none rounded-md p-3 border-gray-300 focus:border-gray-400"
+            rows="5"
+            cols="50"
           ></textarea>
         </label>
 
-        <div className='py-6 flex gap-4 justify-center'>
-          <button
-            className="bg-teal-700 font-semibold hover:bg-stone-500 text-white rounded-md p-2 transition duration-700"
-            type="submit"
-          >
+        <div className="py-6 flex flex-col sm:flex-row gap-4 justify-center">
+          <button className="w-full sm:w-auto bg-black font-semibold text-white rounded-md p-2 transition duration-700">
             Submit
           </button>
 
-          <button className="bg-teal-700 font-semibold text-white rounded-md p-2 hover:bg-stone-500 transition duration-500">
+          <button className="w-full sm:w-auto bg-black font-semibold text-white rounded-md p-2 transition duration-500 mt-2 sm:mt-0">
             Cancel
           </button>
         </div>
-
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
