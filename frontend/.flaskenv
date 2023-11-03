bb1bcd228f0e78326102bad4d72cbaff63af6d52
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-11-03 16:45:00 -0700

    testing flask

---
+++ frontend/.flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=app/api/index.py
+FLASK_DEBUG=False
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,7 +12,18 @@ const nextConfig = {
         pathname: "/"
       }
     ]
-  }
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination:
+        process.env.NODE_ENV === 'development'
+            ? 'http://127.0.0.1:5000/api/:path*'
+            : '/api/',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
