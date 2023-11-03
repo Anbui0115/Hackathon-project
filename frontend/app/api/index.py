cf9cf4b8a7e1a2f04923273caa4ffb2e0df7ee60
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-11-03 16:52:52 -0700

    working flask frontend application

--- frontend/app/api/index.py
+++ frontend/app/api/index.py
@@ -1,50 +1,50 @@
-from .seeds import seed_commands
+# from .seeds import seed_commands
 
 from flask import Flask, request, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
-from .models import db, User
-from .config import Config
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
-from .api.billing_routes import billing_bp
-from .api.about_routes import about_routes
+# from .models import db, User
+# from .config import Config
+# from .api.user_routes import user_routes
+# from .api.auth_routes import auth_routes
+# from .api.billing_routes import billing_bp
+# from .api.about_routes import about_routes
 # from .api.chatbot_routes import chatbot_routes
-from .api.dance_class_registration_routes import dance_class_registration_bp
-from .api.service_appointments_routes import service_appointment_bp
-from .api.testimonial_routes import testimonial_bp
+# from .api.dance_class_registration_routes import dance_class_registration_bp
+# from .api.service_appointments_routes import service_appointment_bp
+# from .api.testimonial_routes import testimonial_bp
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 import os
-from authlib.integrations.flask_client import OAuth
-from backend.oauth_config import configure_oauth
-from .api.google_routes import google_routes
-from .api.media_routes import media_routes
+# from authlib.integrations.flask_client import OAuth
+# from backend.oauth_config import configure_oauth
+# from .api.google_routes import google_routes
+# from .api.media_routes import media_routes
 from dotenv import load_dotenv
 
 app = Flask(__name__)
 load_dotenv()
-app.cli.add_command(seed_commands)
+# app.cli.add_command(seed_commands)
 app.secret_key = os.environ.get('SECRET_KEY')
 # Google OAuth
-app.config.from_object(Config)
-oauth = OAuth(app)
-configure_oauth(app)
+# app.config.from_object(Config)
+# oauth = OAuth(app)
+# configure_oauth(app)
 
 if __name__ == '__main__':
     app.run(debug=False)
 
 # Blueprints
-app.register_blueprint(user_routes, url_prefix='/api/users')
-app.register_blueprint(auth_routes, url_prefix='/api/auth')
-# app.register_blueprint(chatbot_routes, url_prefix='/api/chatbot')
-app.register_blueprint(dance_class_registration_bp, url_prefix='/api/danceclassregistrations')
-app.register_blueprint(service_appointment_bp, url_prefix='/api/serviceappointments')
-app.register_blueprint(about_routes, url_prefix='/api/about')
-app.register_blueprint(billing_bp, url_prefix='/api/billing')
-app.register_blueprint(google_routes, url_prefix='/api/google')
-app.register_blueprint(testimonial_bp, url_prefix='/api/testimonials')
-app.register_blueprint(media_routes, url_prefix='/api/media')
+# app.register_blueprint(user_routes, url_prefix='/api/users')
+# app.register_blueprint(auth_routes, url_prefix='/api/auth')
+# # app.register_blueprint(chatbot_routes, url_prefix='/api/chatbot')
+# app.register_blueprint(dance_class_registration_bp, url_prefix='/api/danceclassregistrations')
+# app.register_blueprint(service_appointment_bp, url_prefix='/api/serviceappointments')
+# app.register_blueprint(about_routes, url_prefix='/api/about')
+# app.register_blueprint(billing_bp, url_prefix='/api/billing')
+# app.register_blueprint(google_routes, url_prefix='/api/google')
+# app.register_blueprint(testimonial_bp, url_prefix='/api/testimonials')
+# app.register_blueprint(media_routes, url_prefix='/api/media')
 
 
 
@@ -53,16 +53,16 @@ login = LoginManager(app)
 login.init_app(app)
 login.login_view = 'auth.unauthorized'
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+# @login.user_loader
+# def load_user(id):
+#     return User.query.get(int(id))
 
 # Configuration
-app.config.from_object(Config)
+# app.config.from_object(Config)
 
 # Database
-db.init_app(app)
-Migrate(app, db)
+# db.init_app(app)
+# Migrate(app, db)
 
 # Application Security
 CORS(app, supports_credentials=True)
@@ -93,3 +93,12 @@ def inject_csrf_token(response):
 @app.route('/test')
 def test():
     return {'message': 'Test Route'}
+
+
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/api/python")
+def hello_world():
+    return "<p>Hello, World!</p>"
